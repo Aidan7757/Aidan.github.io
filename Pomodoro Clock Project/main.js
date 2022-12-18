d3c20799c918cc757e0e268f3acc2707cdef270f
aidanchadha <aidan.chadha@gmail.com>
2022-12-18 00:21:05 -0500

    saving

---
+++ Pomodoro Clock Project/main.js
@@ -0,0 +1,95 @@
+var start = document.getElementById('start');
+var stop = document.getElementById('stop');
+var reset = document.getElementById('reset');
+
+var wm = document.getElementById('w_minutes');
+var ws = document.getElementById('w_seconds');
+
+var bm = document.getElementById('b_minutes');
+var bs = document.getElementById('b_seconds');
+
+var video = document.getElementById('video');
+
+var audio = new Audio("file:///Users/aidanchadha/Desktop/VS%20Code%20Practice%20Work/Pomodoro%20Clock/microwave-timer-117077.mp3")
+
+//function to stop a sound
+function stopSound() {
+    audio.pause();
+}
+//function to play a sound
+function playSound() {
+    audio.play();
+    setTimeout(stopSound, 2000);
+}
+
+//store a reference to a timer variable
+var startTimer;
+
+start.addEventListener('click', function(){
+    if(startTimer === undefined){
+        startTimer = setInterval(timer, 1000)
+    } else {
+        alert("Timer is already running");
+    }
+})
+
+reset.addEventListener('click', function(){
+    wm.innerText = 25;
+    ws.innerText = "00";
+
+    bm.innerText = 5;
+    bs.innerText = "00";
+
+    document.getElementById('counter').innerText = 0;
+    stopInterval()
+    startTimer = undefined;
+})
+
+stop.addEventListener('click', function(){
+    stopInterval()
+    startTimer = undefined;
+})
+
+
+//Start Timer Function
+function timer(){
+    //Work Timer Countdown
+    if(ws.innerText != 0){
+        ws.innerText = parseInt(ws.innerText)
+        ws.innerText--;
+        ws.innerText = ws.innerText.toString().padStart(2, '0');
+    } else if(wm.innerText != 0 && ws.innerText == 0){
+        ws.innerText = 59;
+        wm.innerText--;
+    }
+
+    //Break Timer Countdown
+    if(wm.innerText == 0 && ws.innerText == 0){
+        playSound();
+        if(bs.innerText != 0){
+            bs.innerText = parseInt(bs.innerText)
+            bs.innerText--;
+            bs.innerText = bs.innerText.toString().padStart(2, '0');
+        } else if(bm.innerText != 0 && bs.innerText == 0){
+            bs.innerText = 59;
+            bm.innerText--;
+        }
+    }
+
+    //Increment Counter by one if one full cycle is completed
+    if(wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText == 0){
+        playSound();
+        wm.innerText = 25;
+        ws.innerText = "00";
+
+        bm.innerText = 5;
+        bs.innerText = "00";
+
+        document.getElementById('counter').innerText++;
+    }
+}
+
+//Stop Timer Function
+function stopInterval(){
+    clearInterval(startTimer);
+}
